e098347850df1b0d5f02c22f359922c6d305ff75
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-10-16 03:16:27 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Set client auth mode - true to enable client auth, false to disable it
+const isClientAuthEnabled = false;
+
+/**
+ * Initializes the SDK and sets a global field with passed name for which
+ * it can be referred to later.
+ *
+ * @param {string} name Name by which the chat widget should be referred
+ */
+const initSdk = (name) => {
+    if (!name) {
+        name = 'Bots';          // Set default reference name to 'Bots'
+    }
+    let Bots;
+
+    setTimeout(() => {
+        /**
+            * SDK configuration settings
+            * Other than URI, all fields are optional with two exceptions for auth modes
+            * In client auth disabled mode, 'channelId' must be passed, 'userId' is optional
+            * In client auth enabled mode, 'clientAuthEnabled: true' must be passed
+            */
+        let chatWidgetSettings = {
+            URI: 'oda-2ab8a01762184155804e809ec285e636-da3.data.digitalassistant.oci.oraclecloud.com',
+            clientAuthEnabled: isClientAuthEnabled,
+            channelId: 'fad4c4d1-e921-460b-9913-9036aa07f2a9' 
+        };
+
+        // Initialize SDK
+        if (isClientAuthEnabled) {
+            Bots = new WebSDK(chatWidgetSettings, generateToken);
+        } else {
+            Bots = new WebSDK(chatWidgetSettings);
+        }
+
+        // Connect to the ODA
+        Bots.connect();
+
+        // Create global object to refer Bots
+        window[name] = Bots;
+    }, 0);
+};
